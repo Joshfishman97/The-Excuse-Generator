bb389a0aba09270a7c6136095221f451bf156624
Joshfishman97 <fishmanjosh251@gmail.com>
2021-06-03 15:08:39 +0000

    A message

--- src/app.js
+++ src/app.js
@@ -6,16 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  let who = ["The dog", "My grandma", "His turtle", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "the keys", "the car"];
-  let when = [
-    "before the class",
-    "right on time",
-    "when I finished",
-    "during my lunch",
-    "while I was praying"
-  ];
-  let excuse = who[0] + " " + action[0] + " " + what[0] + " " + when[0];
-  console.log(excuse);
+  function excuse() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
+    let a = Math.floor(Math.random() * who.length);
+    let b = Math.floor(Math.random() * action.length);
+    let c = Math.floor(Math.random() * what.length);
+    let d = Math.floor(Math.random() * when.length);
+    let excuse = who[a] + " " + action[b] + " " + what[c] + " " + when[d];
+    return excuse;
+    console.log(excuse);
+  }
+  let p = document.getElementById("excuse");
+  p.innerHTML = excuse();
 };
